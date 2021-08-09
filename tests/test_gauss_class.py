c6bcd1a19f13376acda6d70f4d59cdfd8087f1fd
nf679 <nf679@york.ac.uk>
2021-08-09 15:02:44 +0100

    fixed test_init

--- tests/test_gauss_class.py
+++ tests/test_gauss_class.py
@@ -19,7 +19,8 @@ class TestGaussClass(unittest.TestCase):
         """
         test_norm1 = norm(loc=5.0, scale=1.0)
         test_norm2 = norm(loc=6.0, scale=0.5)
-        test_gauss = Gauss('../data/test_gauss_data.txt',1.0,10.0)
+        loc_scale = [(5.0,1.0),(6.0,0.5)]
+        test_gauss = Gauss(loc_scale, 1.0, 10.0)
 
     def test_pdf(self):
         test_norm1 = norm(loc=5.0, scale=1.0)
