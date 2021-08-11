c7a2317fd8aea5e6feefd41dde90b644846dbd31
nf679 <nf679@york.ac.uk>
2021-08-11 10:33:17 +0100

    Edited the gauss class init tests

--- tests/test_gauss_class.py
+++ tests/test_gauss_class.py
@@ -17,14 +17,14 @@ class TestGaussClass(unittest.TestCase):
     Tests for the Gauss class.
     """
 
-    def test_init(self):
-        """
-        Test initialisation function.
-        """
-        test_norm1 = norm(loc=5.0, scale=1.0)
-        test_norm2 = norm(loc=6.0, scale=0.5)
-        loc_scale = [(5.0,1.0),(6.0,0.5)]
-        test_gauss = Gauss(loc_scale, 1.0, 10.0)
+    def test_init_locscale(self):
+        assert_equal(test_gauss.data,loc_scale)
+
+    def test_init_lb(self):
+        assert_equal(test_gauss.lb, 1.0)
+
+    def test_init_un(self):
+        assert_equal(test_gauss.ub, 10.0)
 
     def test_pdf(self):
         assert_almost_equal(test_gauss.pdf(1.0), (0.5 * test_norm1.pdf(1.0)) + (0.5 * test_norm2.pdf(1.0)))
